721b832fab185424cfa0d75ed6df98d2dea97248
“Neeru <neeram85@gmail.com>
2020-09-10 16:40:54 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rubygems-update'
+gem 'bundler'
+gem 'sinatra'
+gem 'faraday'
+gem 'json'
+gem 'figaro'
---
+++ app/weather.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup
+require 'sinatra'
+require 'json'
+require 'faraday'
+
+get '/' do
+  
+end
